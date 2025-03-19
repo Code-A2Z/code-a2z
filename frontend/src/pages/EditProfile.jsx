13a49855bde5c86267e677749b9fe8977ddd6277
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-03-19 10:42:51 +0530

    feat: update profile image btn

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,21 +1,26 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../App";
 import axios from "axios";
 import { profileDataStructure } from "./Profile";
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/Loader";
-import { Toaster } from "react-hot-toast";
+import { Toaster, toast } from "react-hot-toast";
 import InputBox from "../components/InputBox";
+import { uploadImage } from "../common/cloudinary";
+import { storeInSession } from "../common/session";
 
 const EditProfile = () => {
 
-    let { userAuth, userAuth: { access_token } } = useContext(UserContext);
+    let { userAuth, userAuth: { access_token }, setUserAuth } = useContext(UserContext);
 
     let bioLimit = 150;
+
+    let profileImgEle = useRef();
 
     const [profile, setProfile] = useState(profileDataStructure);
     const [loading, setLoading] = useState(true);
     const [charactersLeft, setCharactersLeft] = useState(bioLimit);
+    const [updatedProfileImg, setUpdatedProfileImg] = useState(null);
 
     let { personal_info: { fullname, username: profile_username, profile_img, email, bio }, social_links } = profile;
 
@@ -38,6 +43,55 @@ const EditProfile = () => {
         setCharactersLeft(bioLimit - e.target.value.length);
     }
 
+    const handleImagePreview = (e) => {
+
+        let img = e.target.files[0];
+
+        profileImgEle.current.src = URL.createObjectURL(img);
+
+        setUpdatedProfileImg(img);
+    }
+
+    const handleImageUpload = (e) => {
+        e.preventDefault();
+
+        if (updatedProfileImg) {
+
+            let loadingToast = toast.loading("Uploading Image...");
+            e.target.setAttribute("disabled", true);
+
+            uploadImage(updatedProfileImg)
+                .then(url => {
+
+                    if (url) {
+                        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/api/user/update-profile-img", { url: url }, {
+                            headers: {
+                                Authorization: `Bearer ${access_token}`
+                            }
+                        })
+                            .then(({ data }) => {
+                                let newUserAuth = { ...userAuth, profile_img: data.profile_img };
+                                storeInSession("user", JSON.stringify(newUserAuth));
+                                setUserAuth(newUserAuth);
+
+                                setUpdatedProfileImg(null);
+                                toast.dismiss(loadingToast);
+                                e.target.removeAttribute("disabled");
+                                toast.success("Profile Image Updated");
+                            })
+                            .catch(({ response }) => {
+                                toast.dismiss(loadingToast);
+                                e.target.removeAttribute("disabled");
+                                toast.error(response.data.error);
+                            })
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+
     return (
         <AnimationWrapper>
             {
@@ -48,24 +102,30 @@ const EditProfile = () => {
                         <h1 className="max-md:hidden">Edit Profile</h1>
 
                         <div className="flex flex-col lg:flex-row items-start py-10 gap-8 lg:gap-10">
-                            <div className="max-lg:center mb-5">
+                            <div className="max-lg:block max-lg:mx-auto mb-5">
                                 <label htmlFor="uploadImg" id="profileImgLabel" className="relative block w-48 h-48 bg-gray-100 rounded-full overflow-hidden">
                                     <div
                                         className="w-full h-full absolute top-0 left-0 flex items-center justify-center text-white bg-black/80 opacity-0 hover:opacity-100 cursor-pointer">
                                         Upload Image
                                     </div>
-                                    <img src={profile_img} alt="" />
+                                    <img
+                                        ref={profileImgEle}
+                                        src={profile_img}
+                                        alt=""
+                                    />
                                 </label>
 
-                                <input 
-                                    type="file" 
-                                    id="uploadImg" 
-                                    accept=".jpeg, .png, .jpg" 
-                                    hidden 
+                                <input
+                                    onChange={handleImagePreview}
+                                    type="file"
+                                    id="uploadImg"
+                                    accept=".jpeg, .png, .jpg"
+                                    hidden
                                 />
 
                                 <button
-                                    className="btn-light mt-5 max-lg:center lg:w-full px-10"
+                                    onClick={handleImageUpload}
+                                    className="btn-light mt-5 max-lg:block max-lg:mx-auto lg:w-full px-10"
                                 >
                                     Upload
                                 </button>
