8a20748aa9c6c1ea9517eefe2875b1a0bedd16a8
vriti62 <jaitleyvriti@gmail.com>
2025-08-24 01:55:53 +0530

    added-create-collection-manually-and-fixed-the-same

--- backend/Routes/api/collections.routes.js
+++ backend/Routes/api/collections.routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { createNewCollection, saveProject} from '../../Controllers/collection.controller.js';
 import { authenticateUser } from '../../Middlewares/auth.middleware.js';
 const collection = express.Router();
-collection.post("/:id",  authenticateUser, saveProject);
 collection.post("/create-collection", authenticateUser, createNewCollection);
+collection.post("/:id",  authenticateUser, saveProject);
+
 
 export default collection;
