ff5192094fb1404f5cb2c435bb32d10921f88984
vriti62 <jaitleyvriti@gmail.com>
2025-08-29 14:26:58 +0530

    fixed-a-minor-issue

--- backend/Routes/api/collections.routes.js
+++ backend/Routes/api/collections.routes.js
@@ -4,7 +4,7 @@ import { authenticateUser } from '../../Middlewares/auth.middleware.js';
 const collectionRoutes = express.Router();
 collectionRoutes.post("/create-collection", authenticateUser, createNewCollection);
 collectionRoutes.post("/:id",  authenticateUser, saveProject);
-collectionRoutes.post("/delete/:cid/:project_id", authenticateUser, deleteProject);
+collectionRoutes.delete("/delete/:cid/:project_id", authenticateUser, deleteProject);
 
 
 export default collectionRoutes;
