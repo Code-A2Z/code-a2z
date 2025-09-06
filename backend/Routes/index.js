3f0336166da7404efd50dec5a0388c66d2033377
100NikhilBro <100pranjalgupta@gmail.com>
2025-09-06 10:38:57 +0530

    feat: implement rate limiting to prevent brute-force attacks (#1210)

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -8,15 +8,29 @@ import subscriberRoutes from './api/subscriber.routes.js';
 import collectionRoutes from './api/collections.routes.js';
 import collaborationRoutes from './api/collaboration.routes.js';
 
+import { authMiddleware, generalMiddleware } from '../Middlewares/rateLimit/index.js';
+
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
-router.use('/user', userRoutes);
-router.use('/media', mediaRoutes);
-router.use('/project', projectRoutes);
-router.use('/notification', notificationRoutes);
-router.use('/subscriber', subscriberRoutes);
-router.use("/collection", collectionRoutes);
-router.use('/collaboration', collaborationRoutes);
+// router.use('/auth', authRoutes);
+
+router.use('/auth', authMiddleware, authRoutes);
+
+// router.use('/user', userRoutes);
+// router.use('/media', mediaRoutes);
+// router.use('/project', projectRoutes);
+// router.use('/notification', notificationRoutes);
+// router.use('/subscriber', subscriberRoutes);
+// router.use("/collection", collectionRoutes);
+// router.use('/collaboration', collaborationRoutes);
+
+router.use('/user', generalMiddleware, userRoutes);
+router.use('/media', generalMiddleware, mediaRoutes);
+router.use('/project', generalMiddleware, projectRoutes);
+router.use('/notification', generalMiddleware, notificationRoutes);
+router.use('/subscriber', generalMiddleware, subscriberRoutes);
+router.use("/collection", generalMiddleware, collectionRoutes);
+router.use('/collaboration', generalMiddleware, collaborationRoutes);
+
 export default router;
