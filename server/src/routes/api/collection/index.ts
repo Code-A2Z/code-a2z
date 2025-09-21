80ccfaf04cd68b59f881f19888b2200e6fd73421
Avdhesh <114330097+Avdhesh-Varshney@users.noreply.github.com>
2025-09-21 16:51:03 +0000

    update collection apis

--- server/src/routes/api/collection/index.ts
+++ server/src/routes/api/collection/index.ts
@@ -5,11 +5,13 @@ import authenticateUser from '../../../middlewares/auth.middleware';
 
 // Import controller functions
 import createCollection from './controllers/create-collection';
-import saveProjectInCollection from './controllers/save-project-in-collection';
+import saveProjectInCollection from './controllers/save-project';
+import removeProject from './controllers/remove-project';
 
 const collectionRoutes = express.Router();
 
 collectionRoutes.post("/create-collection", authenticateUser, createCollection);
-collectionRoutes.post("/:id",  authenticateUser, saveProjectInCollection);
+collectionRoutes.post("/save-project/:id",  authenticateUser, saveProjectInCollection);
+collectionRoutes.post("/remove-project/:id", authenticateUser, removeProject);
 
 export default collectionRoutes;
