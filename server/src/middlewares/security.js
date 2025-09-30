eb292fc1dc5d429efaac1dbf395c6a5d136a995d
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-01 00:33:04 +0530

    Update security.js

--- server/src/middlewares/security.js
+++ server/src/middlewares/security.js
@@ -2,15 +2,15 @@ import hpp from 'hpp';
 import helmet from 'helmet';
 
 export const securityMiddleware = (server) => {
+  server.use(
+    helmet({
+      hidePoweredBy: true,
+      frameguard: { action: 'deny' },
+      xssFilter: true,
+      noSniff: true,
+      hsts: { maxAge: 31536000, includeSubDomains: true },
+    })
+  );
 
-    server.use(helmet({
-        hidePoweredBy: true,
-        frameguard: { action: "deny" },
-        xssFilter: true,
-        noSniff: true,
-        hsts: { maxAge: 31536000, includeSubDomains: true },
-    }));
-
-    server.use(hpp());
-
-}
+  server.use(hpp());
+};
