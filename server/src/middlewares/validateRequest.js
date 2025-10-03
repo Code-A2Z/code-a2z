fe905fd92aecb1af97c732469e137178ee7ba692
100NikhilBro <100pranjalgupta@gmail.com>
2025-10-04 02:15:19 +0530

    Enhancement: Add centralized express-validator system Issue #1234

---
+++ server/src/middlewares/validateRequest.js
@@ -0,0 +1,15 @@
+import { validationResult } from 'express-validator'
+import { sendResponse } from '../utils/response';
+
+export const validateRequest = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if ((!errors.isEmpty())) {
+        return sendResponse(res, 400, false, "Validation Error", {
+            errors: errors.array().map(err => ({ field: err.param, msg: err.msg }))
+        })
+    }
+
+    next();
+
+}
---
+++ server/src/validators/auth.Validators.js
@@ -0,0 +1,49 @@
+import { body } from 'express-validator'
+import { emailRegex, passwordRegex } from '../utils/regex.js'
+
+
+export const signupValidator = [
+    body('fullname')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Full name should be at least 3 letters long'),
+
+    body('email')
+    .trim()
+    .matches(emailRegex)
+    .withMessage('Invalid email'),
+
+    body('password')
+    .matches(passwordRegex)
+    .withMessage(
+        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+    ),
+]
+
+
+
+export const loginValidator = [
+    body('email')
+    .trim()
+    .matches(emailRegex)
+    .withMessage('Invalid email'),
+
+    body('password')
+    .notEmpty()
+    .withMessage('Password is required'),
+]
+
+
+export const changePasswordValidator = [
+    body('currentPassword')
+    .matches(passwordRegex)
+    .withMessage(
+        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+    ),
+
+    body('newPassword')
+    .matches(passwordRegex)
+    .withMessage(
+        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+    ),
+]
