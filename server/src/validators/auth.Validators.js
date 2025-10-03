6b7681f09324d6e572cd7d23c89afb35306b407f
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-04 02:20:59 +0530

    Update auth.Validators.js

--- server/src/validators/auth.Validators.js
+++ server/src/validators/auth.Validators.js
@@ -1,49 +1,45 @@
 import { body } from 'express-validator'
 import { emailRegex, passwordRegex } from '../utils/regex.js'
 
-
 export const signupValidator = [
-    body('fullname')
+  body('fullname')
     .trim()
     .isLength({ min: 3 })
     .withMessage('Full name should be at least 3 letters long'),
 
-    body('email')
+  body('email')
     .trim()
     .matches(emailRegex)
     .withMessage('Invalid email'),
 
-    body('password')
+  body('password')
     .matches(passwordRegex)
     .withMessage(
-        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+      'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
     ),
 ]
 
-
-
 export const loginValidator = [
-    body('email')
+  body('email')
     .trim()
     .matches(emailRegex)
     .withMessage('Invalid email'),
 
-    body('password')
+  body('password')
     .notEmpty()
     .withMessage('Password is required'),
 ]
 
-
 export const changePasswordValidator = [
-    body('currentPassword')
+  body('currentPassword')
     .matches(passwordRegex)
     .withMessage(
-        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+      'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
     ),
 
-    body('newPassword')
+  body('newPassword')
     .matches(passwordRegex)
     .withMessage(
-        'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
+      'Password should be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
     ),
 ]
