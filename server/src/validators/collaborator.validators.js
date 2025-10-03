f5618cc8391e8ecc7c781558adcbfb12ea4c5330
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-04 02:21:25 +0530

    Update collaborator.validators.js

--- server/src/validators/collaborator.validators.js
+++ server/src/validators/collaborator.validators.js
@@ -1,19 +1,25 @@
 import { body, param } from 'express-validator'
 
 export const inviteCollaboratorValidator = [
-    body('project_id')
-    .notEmpty().withMessage('Project ID is required')
-    .isMongoId().withMessage('Project ID must be a valid Mongo ID'),
+  body('project_id')
+    .notEmpty()
+    .withMessage('Project ID is required')
+    .isMongoId()
+    .withMessage('Project ID must be a valid Mongo ID'),
 ]
 
 export const respondInvitationValidator = [
-    param('token')
-    .notEmpty().withMessage('Token is required')
-    .isHexadecimal().withMessage('Token must be valid'),
+  param('token')
+    .notEmpty()
+    .withMessage('Token is required')
+    .isHexadecimal()
+    .withMessage('Token must be valid'),
 ]
 
 export const getCollaboratorsValidator = [
-    param('project_id')
-    .notEmpty().withMessage('Project ID is required')
-    .isMongoId().withMessage('Project ID must be a valid Mongo ID'),
+  param('project_id')
+    .notEmpty()
+    .withMessage('Project ID is required')
+    .isMongoId()
+    .withMessage('Project ID must be a valid Mongo ID'),
 ]
