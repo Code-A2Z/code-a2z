68b008b354ac53a64873aeea81244fc77f854490
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-04 02:20:15 +0530

    Update validateRequest.js

--- server/src/middlewares/validateRequest.js
+++ server/src/middlewares/validateRequest.js
@@ -1,15 +1,14 @@
 import { validationResult } from 'express-validator'
-import { sendResponse } from '../utils/response';
+import { sendResponse } from '../utils/response'
 
 export const validateRequest = (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req)
 
-    if ((!errors.isEmpty())) {
-        return sendResponse(res, 400, false, "Validation Error", {
-            errors: errors.array().map(err => ({ field: err.param, msg: err.msg }))
-        })
-    }
+  if (!errors.isEmpty()) {
+    return sendResponse(res, 400, false, 'Validation Error', {
+      errors: errors.array().map(err => ({ field: err.param, msg: err.msg })),
+    })
+  }
 
-    next();
-
+  next()
 }
