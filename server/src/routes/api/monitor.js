e8e661f19fd462dfe6857797555d39b030ab6b65
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-04 19:49:44 +0530

    Update monitor.js

--- server/src/routes/api/monitor.js
+++ server/src/routes/api/monitor.js
@@ -3,22 +3,19 @@ import mongoose from 'mongoose';
 
 const router = express.Router();
 
-
 router.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'OK',
-        timestamp: new Date().toISOString()
-    });
-})
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+  });
+});
 
-
-router.get("/db-status", (req, res) => {
-    const dbState = mongoose.connection.readyState;
-    res.status(200).json({
-        dbState,
-        message: dbState === 1 ? 'DB Connected' : 'DB Disconnected'
-    });
-})
-
+router.get('/db-status', (req, res) => {
+  const dbState = mongoose.connection.readyState;
+  res.status(200).json({
+    dbState,
+    message: dbState === 1 ? 'DB Connected' : 'DB Disconnected',
+  });
+});
 
 export default router;
