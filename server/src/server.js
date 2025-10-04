a546944f83531ff1768f5edd9fce1c8ebb53f6c3
100NikhilBro <100pranjalgupta@gmail.com>
2025-10-04 19:37:12 +0530

    Enhancement: Added server monitoring endpoints (#1268)

--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,9 @@ import { securityMiddleware } from './middlewares/security.js';
 // sanitizeFields
 import { sanitizeInput } from './middlewares/sanitizeMiddleware.js';
 
+// MonitorRoutes
+import monitorRoutes from './routes/api/monitor.js';
+
 dotenv.config();
 
 const server = express();
@@ -33,11 +36,19 @@ connectDB();
 
 // Routes
 server.get('/', (req, res) =>
-  res.status(200).json({ status: 'success', message: 'Backend is running...' })
+    res.status(200).json({ status: 'success', message: 'Backend is running...' })
 );
-server.get('/health', (req, res) =>
-  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() })
+
+
+//  monitoring
+
+/* server.get('/health', (req, res) =>
+    res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() })
 );
+*/
+
+server.use('/monitor', monitorRoutes);
+
 server.use('/api', router);
 
 // Error handler (last middleware)
