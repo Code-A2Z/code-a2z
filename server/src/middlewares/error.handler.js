6fa315c6d95eca6d0cb2816afda6dd931f8c31bc
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-06 23:59:14 +0530

    Update error.handler.js

--- server/src/middlewares/error.handler.js
+++ server/src/middlewares/error.handler.js
@@ -2,23 +2,23 @@ import { sendResponse } from '../utils/response.js'
 import logger from '../logger/winston'
 
 const errorHandler = (err, req, res, next) => {
-    if (req ? .logger) {
-        req.logger.error(err.stack || err.message)
-    } else {
-        logger.error(err.stack || err.message)
-    }
+  if (req?.logger) {
+    req.logger.error(err.stack || err.message)
+  } else {
+    logger.error(err.stack || err.message)
+  }
 
-    if (res.headersSent) {
-        return next(err)
-    }
+  if (res.headersSent) {
+    return next(err)
+  }
 
-    return sendResponse(
-        res,
-        500,
-        'error',
-        err.message || 'Internal Server Error',
-        null
-    )
+  return sendResponse(
+    res,
+    500,
+    'error',
+    err.message || 'Internal Server Error',
+    null
+  )
 }
 
 export default errorHandler
